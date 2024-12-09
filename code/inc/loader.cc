092d811323c2dbb6a6b1d26490ae0a399af6a837
unknown <0x646f6e676c696e@proton.me>
2024-12-09 16:04:36 +0800

    fixed a few bugs and tested the binary loader

--- code/inc/loader.cc
+++ code/inc/loader.cc
@@ -12,10 +12,6 @@
 #include "../inc/loader.h"
 
 
-int load_binary(std::string &fname, Binary *bin, Binary::BinaryType type) {
-    return load_binary_bfd(fname, bin, type);
-}
-
 void unload_binary(Binary *bin) {
     size_t i;
     Section *sec;
@@ -63,77 +59,7 @@ static bfd* open_bfd(std::string &fname) {
     return bfd_h;
 }
 
-static int load_binary_bfd(std::string &fname, Binary *bin, Binary::BinaryType type) {
-    int ret;
-    bfd *bfd_h;
-    const bfd_arch_info_type *bfd_info;
-
-    bfd_h = NULL;
-    
-    /* Open the binary and get a bfd handle to it */
-    bfd_h = open_bfd(fname);
-    if(!bfd_h) {
-        goto fail;
-    }
-
-    bin->filename = std::string(fname);
-    /* Get the entry point address, which is a bfd_vma (a 64-bit unsigned integer) */
-    bin->entry    = bfd_get_start_address(bfd_h);
-
-    bin->type_str = std::string(bfd_h->xvec->name);
-    switch(bfd_h->xvec->flavour) {
-        case bfd_target_elf_flavour:
-            bin->type = Binary::BIN_TYPE_ELF;
-            break;
-        case bfd_target_coff_flavour:
-            bin->type = Binary::BIN_TYPE_PE;
-            break;
-        case bfd_target_unknown_flavour:
-        default:
-            fprint(stderr, "unsupported binary type (%s)\n", bfd_h->xvec->name);
-            goto fail;
-    }
-
-    /* Get the architecture */
-    bfd_info = bfd_get_arch_info(bfd_h);
-    bin->arch_str = std::string(bfd_info->printable_name);
-    switch(bfd_info->mach) {
-        case bfd_mach_i386_i386:
-            bin->arch = Binary::ARCH_X86;
-            bin->bits = 32;
-            break;
-        case bfd_mach_x86_64:
-            bin->arch = Binary::ARCH_X86;
-            bin->bits = 64;
-            break;
-        default:
-            fprint(stderr, "unsupported architecture (%s)\n", bfd_info->printable_name);
-            goto fail;
-    }
-
-    /* Symbol handling is best-effort only (they may not even be present) */
-    load_symbols_bfd(bfd_h, bin);
-    load_dynsym_bfd(bfd_h, bin);
-
-    if(load_sections_bfd(bfd_h, bin) < 0) {
-        goto fail;
-    }
-
-    ret = 0;
-    goto cleanup;
-
-    fail:
-    return -1;
-
-    cleanup:
-    if(bfd_h) {
-        bfd_close(bfd_h);
-    }
-
-    return ret;
-}
-
-static int load_symols_bfd(bfd *bfd_h, Binary *bin) {
+static int load_symbols_bfd(bfd *bfd_h, Binary *bin) {
     int ret;
     long n, nsyms, i;
     /* In libbfd, symbols are represented by the asymbol structure, which is just a short name for struct bfd_symbol */
@@ -146,14 +72,14 @@ static int load_symols_bfd(bfd *bfd_h, Binary *bin) {
     n = bfd_get_symtab_upper_bound(bfd_h);
 
     if(n < 0) {
-        fprint(stderr, "failed to read symtab (%s)\n", bfd_errmsg(bfd_get_error())));
+        fprintf(stderr, "failed to read symtab (%s)\n", bfd_errmsg(bfd_get_error()));
         goto fail;
     }
     else if(n) {
         /* Malloc enough space for all symbols */
         bfd_symtab = (asymbol**) malloc(n);
         if(!bfd_symtab) {
-            fprint(stderr, "out of memory\n");
+            fprintf(stderr, "out of memory\n");
             goto fail;
         }
 
@@ -161,17 +87,17 @@ static int load_symols_bfd(bfd *bfd_h, Binary *bin) {
         nsyms = bfd_canonicalize_symtab(bfd_h, bfd_symtab);
 
         if(nsyms < 0) {
-            fprint(stderr, "failed to read symtab (%s)\n", bfd_errmsg(bfd_get_error()));
+            fprintf(stderr, "failed to read symtab (%s)\n", bfd_errmsg(bfd_get_error()));
             goto fail;
         }
 
         for(i = 0; i < nsyms; i++) {
             /* For this binary loader, we are interested only in function symbols */
             if(bfd_symtab[i]->flags & BSF_FUNCTION) {
-                bin->symols.push_back(Symbol());
+                bin->symbols.push_back(Symbol());
                 sym = &bin->symbols.back();
                 sym->type = Symbol::SYM_TYPE_FUNC;
-                sym->name = std:string(bfd_symtab[i]->name);
+                sym->name = std::string(bfd_symtab[i]->name);
                 /* Get a function symbol's value, which is the start address */
                 sym->addr = bfd_asymbol_value(bfd_symtab[i]);
             }
@@ -204,30 +130,30 @@ static int load_dynsym_bfd(bfd *bfd_h, Binary *bin) {
     n = bfd_get_dynamic_symtab_upper_bound(bfd_h);
 
     if(n < 0) {
-        fprint(stderr, "failed to read dynamic symtab (%s)\n", bfd_errmsg(bfd_get_error())));
+        fprintf(stderr, "failed to read dynamic symtab (%s)\n", bfd_errmsg(bfd_get_error()));
         goto fail;
     }
     else if(n) {
         bfd_dynsym = (asymbol**) malloc(n);
         if(!bfd_dynsym) {
-            fprint(stderr, "out of memory\n");
+            fprintf(stderr, "out of memory\n");
             goto fail;
         }
 
         nsyms = bfd_canonicalize_dynamic_symtab(bfd_h, bfd_dynsym);
 
         if(nsyms < 0) {
-            fprint(stderr, "failed to read symtab (%s)\n", bfd_errmsg(bfd_get_error()));
+            fprintf(stderr, "failed to read symtab (%s)\n", bfd_errmsg(bfd_get_error()));
             goto fail;
         }
 
         for(i = 0; i < nsyms; i++) {
             
             if(bfd_dynsym[i]->flags & BSF_FUNCTION) {
-                bin->symols.push_back(Symbol());
+                bin->symbols.push_back(Symbol());
                 sym = &bin->symbols.back();
                 sym->type = Symbol::SYM_TYPE_FUNC;
-                sym->name = std:string(bfd_dynsym[i]->name);
+                sym->name = std::string(bfd_dynsym[i]->name);
                 sym->addr = bfd_asymbol_value(bfd_dynsym[i]);
             }
         }
@@ -240,8 +166,8 @@ static int load_dynsym_bfd(bfd *bfd_h, Binary *bin) {
     return -1;
 
     cleanup:
-    if(bfd_symtab) {
-        free(bfd_symtab);
+    if(bfd_dynsym) {
+        free(bfd_dynsym);
     }
     return ret;
 }
@@ -256,7 +182,7 @@ static int load_sections_bfd(bfd *bfd_h, Binary *bin) {
     Section::SectionType sectype;
 
     for(bfd_sec = bfd_h->sections; bfd_sec; bfd_sec = bfd_sec->next) {
-        bfd_flags = bfd_get_flags(bfd_h, bfd_sec);
+        bfd_flags = bfd_get_section_flags(bfd_h, bfd_sec);
 
         sectype = Section::SEC_TYPE_NONE;
 
@@ -301,4 +227,78 @@ static int load_sections_bfd(bfd *bfd_h, Binary *bin) {
     }
 
     return 0;
+}
+
+static int load_binary_bfd(std::string &fname, Binary *bin, Binary::BinaryType type) {
+    int ret;
+    bfd *bfd_h;
+    const bfd_arch_info_type *bfd_info;
+
+    bfd_h = NULL;
+    
+    /* Open the binary and get a bfd handle to it */
+    bfd_h = open_bfd(fname);
+    if(!bfd_h) {
+        goto fail;
+    }
+
+    bin->filename = std::string(fname);
+    /* Get the entry point address, which is a bfd_vma (a 64-bit unsigned integer) */
+    bin->entry    = bfd_get_start_address(bfd_h);
+
+    bin->type_str = std::string(bfd_h->xvec->name);
+    switch(bfd_h->xvec->flavour) {
+        case bfd_target_elf_flavour:
+            bin->type = Binary::BIN_TYPE_ELF;
+            break;
+        case bfd_target_coff_flavour:
+            bin->type = Binary::BIN_TYPE_PE;
+            break;
+        case bfd_target_unknown_flavour:
+        default:
+            fprintf(stderr, "unsupported binary type (%s)\n", bfd_h->xvec->name);
+            goto fail;
+    }
+
+    /* Get the architecture */
+    bfd_info = bfd_get_arch_info(bfd_h);
+    bin->arch_str = std::string(bfd_info->printable_name);
+    switch(bfd_info->mach) {
+        case bfd_mach_i386_i386:
+            bin->arch = Binary::ARCH_X86;
+            bin->bits = 32;
+            break;
+        case bfd_mach_x86_64:
+            bin->arch = Binary::ARCH_X86;
+            bin->bits = 64;
+            break;
+        default:
+            fprintf(stderr, "unsupported architecture (%s)\n", bfd_info->printable_name);
+            goto fail;
+    }
+
+    /* Symbol handling is best-effort only (they may not even be present) */
+    load_symbols_bfd(bfd_h, bin);
+    load_dynsym_bfd(bfd_h, bin);
+
+    if(load_sections_bfd(bfd_h, bin) < 0) {
+        goto fail;
+    }
+
+    ret = 0;
+    goto cleanup;
+
+    fail:
+    return -1;
+
+    cleanup:
+    if(bfd_h) {
+        bfd_close(bfd_h);
+    }
+
+    return ret;
+}
+
+int load_binary(std::string &fname, Binary *bin, Binary::BinaryType type) {
+    return load_binary_bfd(fname, bin, type);
 }
